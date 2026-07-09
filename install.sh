8bf625a726182ac5f492ebf42b6b512592b00c79
zamuwelle <xopxtech@gmail.com>
2026-07-10 05:53:22 +0800

    chore: fix typo for vscode and fix wildcard issue

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ current_dir="$(dirname "$(dirname "$(readlink -f "$0")")")/dotfiles"
 vscode_dir="$HOME/.config/Code/User"
 neovim_dir="$HOME/.config/nvim"
 
-mkdir -p "$vcode_dir"
+mkdir -p "$vscode_dir"
 mkdir -p "$neovim_dir"
 
-cp -f -r "$current_dir/Code/*" "$vscode_dir"
-cp -f -r "$current_dir/nvim/*" "$neovim_dir"
+cp -f -r "$current_dir/Code"/* "$vscode_dir"
+cp -f -r "$current_dir/nvim"/* "$neovim_dir"
 
-rm -f -r $current_dir
+rm -f -r "$current_dir"
