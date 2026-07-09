bdfa194db7057102a871fcb44ee6b8feb2d00735
zamuwelle <xopxtech@gmail.com>
2026-07-10 05:50:53 +0800

    chore: add recursive parameters

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ neovim_dir="$HOME/.config/nvim"
 mkdir -p "$vcode_dir"
 mkdir -p "$neovim_dir"
 
-cp -f "$current_dir/Code/*" "$vscode_dir"
-cp -f "$current_dir/nvim/*" "$neovim_dir"
+cp -f -r "$current_dir/Code/*" "$vscode_dir"
+cp -f -r "$current_dir/nvim/*" "$neovim_dir"
 
 rm -f -r $current_dir
