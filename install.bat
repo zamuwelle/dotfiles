fe9018ad96f06bb9a5689af270727c915a81acae
zamuwelle <xopxtech@gmail.com>
2026-07-11 00:35:21 +0800

    chore: Fix wrong variable

--- install.bat
+++ install.bat
@@ -1,13 +1,13 @@
 @echo off
 
-for %%I in ("%~dp0..\..") do set "current_dir=%%~fI\dotfiles\"
+for %%I in ("%~dp0..\..") do set "currentDir=%%~fI\dotfiles\"
 set "vscodeDir=%APPDATA%\Code\User\"
 set "neovimDir=%LOCALAPPDATA%\nvim\"
 
 md "%vscodeDir%"
 md "%neovimDir%"
 
-copy /y "%current_dir%Code\*" "%vscode_dir%"
-copy /y "%current_dir%nvim\*" "%vscode_dir%"
+copy /y "%currentDir%Code\*" "%vscodeDir%"
+copy /y "%currentDir%nvim\*" "%vscodeDir%"
 
 rmdir /q /s "%currentDir%"
