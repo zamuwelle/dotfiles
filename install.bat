3ada9e77a72c0de458f671e7b8ad10c24257d94a
zamuwelle <xopxtech@gmail.com>
2026-07-11 01:12:19 +0800

    chore: fix path

--- install.bat
+++ install.bat
@@ -1,13 +1,13 @@
 @echo off
 
 for %%I in ("%~dp0..") do set "currentDir=%%~fI\dotfiles"
-set "vscodeDir=%APPDATA%\Code\User\"
-set "neovimDir=%LOCALAPPDATA%\nvim\"
+set "vscodeDir=%APPDATA%\Code\User"
+set "neovimDir=%LOCALAPPDATA%\nvim"
 
 md "%vscodeDir%"
 md "%neovimDir%"
 
-robocopy "%currentDir%Code" "%vscodeDir%" /e
-robocopy "%currentDir%nvim" "%neovimDir%" /e
+robocopy "%currentDir%\Code" "%vscodeDir%" /e
+robocopy "%currentDir%\nvim" "%neovimDir%" /e
 
 rmdir /q /s "%currentDir%"
