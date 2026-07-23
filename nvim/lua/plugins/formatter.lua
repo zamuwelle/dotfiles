6b60dc22df094d132e6d53ce0eb8392c75460f44
zamuwelle <xopxtech@gmail.com>
2026-07-23 16:45:53 +0800

    chore: add better comment keybinds
    Supports single-line and multi-line comments

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,5 +1,31 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	opts = {},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({})
+
+			vim.keymap.set("n", "<C-\\>", function()
+				require("Comment.api").toggle.linewise.current()
+			end, { desc = "Smart comment" })
+
+			vim.keymap.set("v", "<C-\\>", function()
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "nx", false)
+
+				local start_line = vim.api.nvim_buf_get_mark(0, "<")[1]
+				local end_line = vim.api.nvim_buf_get_mark(0, ">")[1]
+				local line_count = math.abs(end_line - start_line) + 1
+
+				if line_count > 1 then
+					require("Comment.api").toggle.blockwise("V")
+				else
+					require("Comment.api").toggle.linewise("V")
+				end
+			end, { desc = "Smart comment" })
+		end,
+	},
 }
